c8e6d0b79fb1911309250379b4b87e1780fbb30a
Colin <why101@msn.com>
2019-11-05 09:56:17 -0800

    fixed file resolution in PluginFile

--- src/elements/PluginFile.js
+++ src/elements/PluginFile.js
@@ -1,14 +1,16 @@
 const {getFileName} = require('../helpers');
+const Container = require('./Container');
 const MemoryMap = require('memory-map');
 const GroupRecord = require('./GroupRecord');
 const MainRecord = require('./MainRecord');
 
-class PluginFile {
-    constructor(filePath, init = true) {
+class PluginFile extends Container {
+    constructor(session, filePath, init = true) {
+        super();
+        this.session = session;
         this.filePath = filePath;
         this.fileName = getFileName(filePath);
         this.file = this;
-        this._groups = [];
         if (init) this.init();
     }
 
@@ -18,6 +20,7 @@ class PluginFile {
         plugin.fileSize = plugin.memoryMap.getSize();
         plugin.parseFileHeader();
         plugin.parseGroups();
+        plugin.fileManager.addFile(this);
         return plugin;
     }
 
@@ -30,14 +33,14 @@ class PluginFile {
         let masterFileElements = this.fileHeader.getElements('Masters Files');
         this._masters = masterFileElements.map(masterFileElement => {
             let filename = masterFileElement.getValue('MAST');
-            return getFileByName(filename, true);
+            return this.fileManager.getFileByName(filename, true);
         });
     }
 
     parseGroups() {
         this.memoryMap.setPos(this.fileHeader.nextOffset);
         while (this.memoryMap.getPos() < this.fileSize)
-            this._groups.push(GroupRecord.load(this));
+            GroupRecord.load(this);
     }
 
     get fileHeader() {
@@ -52,9 +55,17 @@ class PluginFile {
         return this._groups.slice();
     }
 
+    get fileManager() {
+        return this.session.fileManager;
+    }
+
+    get definitionManager() {
+        return this.session.definitionManager;
+    }
+
     init() {
         // TODO: initialize ESM/ESL flag based on filename
-        this._fileHeader = new MainRecord('TES4');
+        this._fileHeader = new MainRecord(this, 'TES4');
     }
 
     // TODO: verify next id isn't in use
