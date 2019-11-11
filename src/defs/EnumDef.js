a38ce69692c5a7447c5647ae7db26b9bece57ad9
Colin <why101@msn.com>
2019-11-11 13:29:51 -0800

    added enum def

---
+++ src/defs/EnumDef.js
@@ -0,0 +1,24 @@
+const Def = require('./Def');
+const InvalidEnumValueError = require('../errors/InvalidEnumValueError');
+
+const unknownExpr = /^(?:Unknown )?(-?\d+)$/;
+
+class EnumDef extends Def {
+    dataToValue(element, data) {
+        return this.options[data] || `Unknown ${data}`;
+    }
+
+    valueToData(element, value) {
+        let opt = Object.keys(this.options).find(key => {
+            return this.options[key] === value;
+        });
+        if (opt !== undefined) return parseInt(opt);
+        let match = value.match(unknownExpr);
+        if (match) return parseInt(match[1]);
+        throw new InvalidEnumValueError(value);
+    }
+}
+
+module.exports = Object.assign(EnumDef, {
+    defType: 'enum'
+});
