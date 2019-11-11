b1c5c82dec5fe32b9f312c62f4dc509c2c6d83ac
Colin <why101@msn.com>
2019-11-11 13:31:29 -0800

    added write function to valueDef

--- src/defs/ValueDef.js
+++ src/defs/ValueDef.js
@@ -2,6 +2,9 @@ const Def = require('./Def');
 const ValueElement = require('../elements/ValueElement');
 
 class ValueDef extends Def {
+    write(data, stream) {
+        stream.write(this.toBytes(data));
+    }
 }
 
 module.exports = Object.assign(ValueDef, {
