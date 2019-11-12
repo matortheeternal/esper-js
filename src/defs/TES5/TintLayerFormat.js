4f01873b53db9a1b05b2e7ce997ad61d75d61f0e
Colin <why101@msn.com>
2019-11-12 12:29:36 -0800

    added TintLayerFormat

---
+++ src/defs/TES5/TintLayerFormat.js
@@ -0,0 +1,19 @@
+const FormatDef = require('../FormatDef');
+const {getTintLayerName} = require('../../tintLayerCache');
+
+class TintLayerFormat extends FormatDef {
+    dataToValue(element, data) {
+        let actor = element.record,
+            race = actor.getElement('@RNAM'),
+            female = actor.getFlag('ACBS\\Flags', 'Female');
+        if (!race) return `${data}`;
+        let name = getTintLayerName(race, female, data);
+        return `${data} ${name}`;
+    }
+
+    valueToData(element, value) {
+        return parseInt(value);
+    }
+}
+
+module.exports = TintLayerFormat;
