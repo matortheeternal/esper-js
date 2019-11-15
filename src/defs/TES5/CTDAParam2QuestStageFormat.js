a51a01ca8a7881c59ac629685bcda9223d501dec
Colin <why101@msn.com>
2019-11-14 18:18:44 -0800

    added quest stage formats

---
+++ src/defs/TES5/CTDAParam2QuestStageFormat.js
@@ -0,0 +1,6 @@
+const FormatDef = require('../FormatDef');
+
+class CTDAParam2QuestStageFormat extends FormatDef {
+}
+
+module.exports = CTDAParam2QuestStageFormat;
---
+++ src/defs/TES5/PerkDATAQuestStageFormat.js
@@ -0,0 +1,6 @@
+const FormatDef = require('../FormatDef');
+
+class PerkDATAQuestStageFormat extends FormatDef {
+}
+
+module.exports = PerkDATAQuestStageFormat;
