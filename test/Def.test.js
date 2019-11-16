618ac6dca047ef39d08e9653bf9055ad78c0fa6e
Colin <why101@msn.com>
2019-11-15 17:19:40 -0800

    added Def tests

---
+++ test/Def.test.js
@@ -0,0 +1,50 @@
+const DefinitionManager = require('../src/DefinitionManager');
+const Def = require('../src/defs/Def');
+
+describe('Def', () => {
+    let manager;
+
+    beforeAll(() => {
+        manager = new DefinitionManager('TES5');
+    });
+
+    describe('constructor', () => {
+        let def;
+
+        it('should be defined', () => {
+            expect(Def).toBeDefined();
+        });
+
+        it('should create a new instance', () => {
+            def = new Def(manager, {prop: 1}, null);
+            expect(def).toBeInstanceOf(Def);
+        });
+
+        it(`should assign the def's properties to the class instance`, () => {
+            expect(def.prop).toBe(1);
+        });
+
+        it('should assign parent if truthy', () => {
+            expect(def.hasOwnProperty('parent')).toBe(false);
+            let parent = def;
+            def = new Def(manager, {}, parent);
+            expect(def.parent).toBe(parent);
+        });
+    });
+
+    describe('containsSignature', () => {
+        let def;
+
+        beforeAll(() => {
+            def = new Def(manager, {}, null);
+        });
+
+        it('should be defined', () => {
+            expect(def.containsSignature).toBeDefined();
+        });
+
+        it('should return false', () => {
+            expect(def.containsSignature('ABCD')).toBe(false);
+        });
+    });
+});
