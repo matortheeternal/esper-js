98d67a7ebe3bf951a9fd1b1d55057ce4646056d7
Colin <why101@msn.com>
2019-11-15 17:38:48 -0800

    added EnumDef tests

---
+++ test/EnumDef.test.js
@@ -0,0 +1,94 @@
+const DefinitionManager = require('../src/DefinitionManager');
+const EnumDef = require('../src/defs/EnumDef');
+const FormatDef = require('../src/defs/FormatDef');
+const InvalidEnumValueError = require('../src/errors/InvalidEnumValueError');
+
+const animalEnum = {
+    options: {
+        1: 'Giraffe',
+        2: 'Hippo'
+    },
+    unknownOption: '<Unknown Animal>'
+};
+
+describe('EnumDef', () => {
+    let manager;
+
+    beforeAll(() => {
+        manager = new DefinitionManager('TES5');
+    });
+
+    describe('constructor', () => {
+        let def;
+
+        it('should be defined', () => {
+            expect(EnumDef).toBeDefined();
+        });
+
+        it('should create a new instance', () => {
+            def = new EnumDef(manager, {}, null);
+            expect(def).toBeInstanceOf(EnumDef);
+        });
+
+        it('should extend FormatDef', () => {
+            expect(EnumDef.prototype).toBeInstanceOf(FormatDef);
+        });
+    });
+
+    describe('dataToValue', () => {
+        let def;
+
+        beforeAll(() => {
+            def = new EnumDef(manager, animalEnum, null);
+        });
+
+        it('should be defined', () => {
+            expect(def.dataToValue).toBeDefined();
+        });
+
+        describe('known option', () => {
+            it('should return the enumeration option value', () => {
+                expect(def.dataToValue(null, 1)).toBe('Giraffe');
+                expect(def.dataToValue(null, 2)).toBe('Hippo');
+            });
+        });
+
+        describe('unknown option', () => {
+            it('should return the unknownOption string if provided', () => {
+                expect(def.dataToValue(null, 3)).toBe('<Unknown Animal>');
+            });
+
+            it('should return default format if unknownOption not provided', () => {
+                delete def.unknownOption;
+                expect(def.dataToValue(null, 3)).toBe('<Unknown 3>');
+            });
+        });
+    });
+
+    describe('valueToData', () => {
+        let def;
+
+        beforeAll(() => {
+            def = new EnumDef(manager, animalEnum, null);
+        });
+
+        describe('known option', () => {
+            it('should return the option id', () => {
+                expect(def.valueToData(null, 'Giraffe')).toBe(1);
+                expect(def.valueToData(null, 'Hippo')).toBe(2);
+            });
+        });
+
+        describe('unknown option', () => {
+            it('should return number if in default format', () => {
+                expect(def.valueToData(null, '<Unknown 3>')).toBe(3);
+            });
+
+            it('should throw an error if not in default format', () => {
+                expect(() => {
+                    def.valueToData(null, '<Unknown Animal>');
+                }).toThrow(InvalidEnumValueError);
+            });
+        });
+    });
+});
