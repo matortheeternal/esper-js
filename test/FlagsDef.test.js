10759910d83e090a6d41123ac279a0f7b12ab060
Colin <why101@msn.com>
2019-11-16 11:39:58 -0800

    added FlagsDef tests

---
+++ test/FlagsDef.test.js
@@ -0,0 +1,100 @@
+const DefinitionManager = require('../src/DefinitionManager');
+const FlagsDef = require('../src/defs/FlagsDef');
+const FormatDef = require('../src/defs/FormatDef');
+const UInt32Def = require('../src/defs/UInt32Def');
+
+const exampleFlags = {
+    flags: {
+        0: 'Initially Disabled',
+        1: 'No Havok'
+    }
+};
+
+describe('FlagsDef', () => {
+    let manager;
+
+    beforeAll(() => {
+        manager = new DefinitionManager('TES5');
+    });
+
+    describe('constructor', () => {
+        it('should be defined', () => {
+            expect(FlagsDef).toBeDefined();
+        });
+
+        it('should create a new instance', () => {
+            let def = new FlagsDef(manager, {}, null);
+            expect(def).toBeInstanceOf(FlagsDef);
+        });
+
+        it('should extend FormatDef', () => {
+            expect(FlagsDef.prototype).toBeInstanceOf(FormatDef);
+        });
+    });
+
+    describe('instance methods', () => {
+        let def;
+
+        beforeAll(() => {
+            let uint32Def = new UInt32Def(manager, {}, null);
+            def = new FlagsDef(manager, exampleFlags, uint32Def);
+        });
+
+        describe('getFlagValue', () => {
+            it('should be defined', () => {
+                expect(def.getFlagValue).toBeDefined();
+            });
+
+            it(`should return the flag value`, () => {
+                expect(def.getFlagValue(0)).toBe('Initially Disabled');
+                expect(def.getFlagValue(1)).toBe('No Havok');
+            });
+
+            it('should return unknown flag values', () => {
+                expect(def.getFlagValue(3)).toBe('Unknown 3');
+            });
+        });
+
+        describe('getFlagIndex', () => {
+            it('should be defined', () => {
+                expect(def.getFlagIndex).toBeDefined();
+            });
+
+            it(`should return the flag index for known flags`, () => {
+                expect(def.getFlagIndex('Initially Disabled')).toBe(0);
+                expect(def.getFlagIndex('No Havok')).toBe(1);
+            });
+
+            it('should return the flag index for unknown flags', () => {
+                expect(def.getFlagIndex('Unknown 3')).toBe(3);
+            });
+        });
+
+        describe('dataToValue', () => {
+            it('should be defined', () => {
+                expect(def.dataToValue).toBeDefined();
+            });
+
+            it('should return single flag values', () => {
+                expect(def.dataToValue(null, 0x1)).toBe('Initially Disabled');
+                expect(def.dataToValue(null, 0x2)).toBe('No Havok');
+            });
+
+            it('should return multiple flag values separated by commas', () => {
+                expect(def.dataToValue(null, 0x3)).toBe(
+                    'Initially Disabled, No Havok'
+                );
+            });
+
+            it('should return the unknown flag values', () => {
+                expect(def.dataToValue(null, 12)).toBe('Unknown 2, Unknown 3');
+            });
+        });
+
+        describe('valueToData', () => {
+            it('should be defined', () => {
+                expect(def.valueToData).toBeDefined();
+            });
+        });
+    });
+});
