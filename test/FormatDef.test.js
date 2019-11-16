71e4ac950da8af8f55879e9f76ee6b0ee76840bc
Colin <why101@msn.com>
2019-11-15 17:28:04 -0800

    added FormatDef tests

---
+++ test/FormatDef.test.js
@@ -0,0 +1,60 @@
+const DefinitionManager = require('../src/DefinitionManager');
+const FormatDef = require('../src/defs/FormatDef');
+const Def = require('../src/defs/Def');
+
+describe('FormatDef', () => {
+    let manager;
+
+    beforeAll(() => {
+        manager = new DefinitionManager('TES5');
+    });
+
+    describe('constructor', () => {
+        let def;
+
+        it('should be defined', () => {
+            expect(FormatDef).toBeDefined();
+        });
+
+        it('should create a new instance', () => {
+            def = new FormatDef(manager, {}, null);
+            expect(def).toBeInstanceOf(FormatDef);
+        });
+
+        it('should extend def', () => {
+            expect(FormatDef.prototype).toBeInstanceOf(Def);
+        });
+    });
+
+    describe('dataToValue', () => {
+        let def;
+
+        beforeAll(() => {
+            def = new FormatDef(manager, {}, null);
+        });
+
+        it('should be defined', () => {
+            expect(def.dataToValue).toBeDefined();
+        });
+
+        it('should return the data converted to a string', () => {
+            expect(def.dataToValue(null, 40)).toBe('40');
+        });
+    });
+
+    describe('valueToData', () => {
+        let def;
+
+        beforeAll(() => {
+            def = new FormatDef(manager, {}, null);
+        });
+
+        it('should be defined', () => {
+            expect(def.valueToData).toBeDefined();
+        });
+
+        it('should return the value converted to an integer', () => {
+            expect(def.valueToData(null, '40')).toBe(40);
+        });
+    });
+});
