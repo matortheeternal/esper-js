c11bcf2ba136b2bf16dd9522084b92c7bce30706
Colin <why101@msn.com>
2019-11-15 17:18:36 -0800

    added unimplemented toBytes function to ValueDef

--- src/defs/ValueDef.js
+++ src/defs/ValueDef.js
@@ -1,9 +1,14 @@
 const Def = require('./Def');
 const ValueElement = require('../elements/ValueElement');
+const UnimplementedError = require('../errors/UnimplementedError');
 
 class ValueDef extends Def {
     write(data, stream) {
         stream.write(this.toBytes(data));
+    }
+
+    toBytes(data) {
+        throw new UnimplementedError();
     }
 }
 
