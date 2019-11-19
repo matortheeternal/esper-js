0c39d11d4b55f044f6c5c64f6a4240d798baf7e7
Colin <why101@msn.com>
2019-11-18 16:31:23 -0800

    FormIdDef now throws an error if a format is provided

--- src/defs/FormIdDef.js
+++ src/defs/FormIdDef.js
@@ -2,6 +2,11 @@ const UInt32Def = require('./UInt32Def');
 const FormIdValue = require('../FormIdValue');
 
 class FormIdDef extends UInt32Def {
+    constructor(manager, def, parent) {
+        if (def.format) throw new Error('FormId cannot have format.');
+        super(manager, def, parent);
+    }
+
     getValue(element) {
         return FormIdValue.fromElement(element);
     }
