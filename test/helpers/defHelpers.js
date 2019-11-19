b8cf043e3555fb4b5d02ef84b7d4b8e80ee41dd2
Colin <why101@msn.com>
2019-11-18 22:57:42 -0800

    added flags def helper

--- test/helpers/defHelpers.js
+++ test/helpers/defHelpers.js
@@ -2,7 +2,8 @@ let subrecord = (signature, element) => ({signature, element, type: 'subrecord'}
 let uint32 = (name) => ({name, type: 'uint32'});
 let float = (name) => ({name, type: 'float'});
 let string = (name) => ({name, type: 'string'});
+let flags = (flags) => ({flags, type: 'flags'});
 
 module.exports = {
-    subrecord, uint32, float, string
+    subrecord, uint32, float, string, flags
 };
