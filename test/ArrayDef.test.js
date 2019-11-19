009851032f169e045a2239d5a17a31b0edbb9a8d
Colin <why101@msn.com>
2019-11-18 17:27:33 -0800

    added ArrayDef tests

---
+++ test/ArrayDef.test.js
@@ -0,0 +1,60 @@
+const DefinitionManager = require('../src/DefinitionManager');
+const ArrayDef = require('../src/defs/ArrayDef');
+const Def = require('../src/defs/Def');
+const ArrayElement = require('../src/elements/ArrayElement');
+
+const basicArray = {
+    name: 'Basic Array',
+    element: {
+        name: 'Number',
+        type: 'uint32'
+    }
+};
+
+describe('ArrayDef', () => {
+    let manager;
+
+    beforeAll(() => {
+        manager = new DefinitionManager('TES5');
+    });
+
+    describe('constructor', () => {
+        let def;
+
+        it('should be defined', () => {
+            expect(ArrayDef).toBeDefined();
+        });
+
+        it('should extend Def', () => {
+            expect(ArrayDef.prototype).toBeInstanceOf(Def);
+        });
+
+        it('should create a new instance', () => {
+            def = new ArrayDef(manager, basicArray, null);
+            expect(def).toBeInstanceOf(ArrayDef);
+        });
+
+        it('should set elementDef', () => {
+            expect(def.elementDef).toBeDefined();
+            expect(def.elementDef).toBeInstanceOf(Def);
+        });
+    });
+
+    describe('instance methods', () => {
+        let def;
+
+        beforeAll(() => {
+            def = new ArrayDef(manager, basicArray, null);
+        });
+
+        describe('initElement', () => {
+            it('should return a new ArrayElement', () => {
+                let a = def.initElement();
+                expect(a).toBeDefined();
+                expect(a).toBeInstanceOf(ArrayElement);
+                expect(a.def).toBe(def);
+                expect(a.container).toBeUndefined();
+            });
+        });
+    });
+});
