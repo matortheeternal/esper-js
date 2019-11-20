6256f5dabd2c2a07c49317a6b2bb60b62f0819f0
Colin <why101@msn.com>
2019-11-20 09:10:10 -0800

    implemented FormatUnionDef

--- src/defs/FormatUnionDef.js
+++ src/defs/FormatUnionDef.js
@@ -1,7 +1,30 @@
-const Def = require('./Def');
+const FormatDef = require('./FormatDef');
+const {ExpectedDefPropertyError} = require('../errors');
 
-class FormatUnionDef extends Def {
+class FormatUnionDef extends FormatDef {
+    constructor(manager, def, parent) {
+        super(manager, def, parent);
+        if (!def.decider) throw new ExpectedDefPropertyError(def, 'decider');
+        this.decide = manager.resolveDef(def.decider);
+        if (!def.formats) throw new ExpectedDefPropertyError(def, 'formats');
+        this.formatDefs = manager.buildDefs(def.formats);
+    }
 
+    getFormat(element) {
+        let index = this.decide(element) - 1;
+        if (index === -1) throw new Error('Could not resolve union.');
+        return this.formatDefs[index];
+    }
+
+    dataToValue(element, data) {
+        let format = this.getFormat(element);
+        return format.dataToValue(element, data);
+    }
+
+    valueToData(element, value) {
+        let format = this.getFormat(element);
+        return format.valueToData(element, value);
+    }
 }
 
 module.exports = Object.assign(FormatUnionDef, {
