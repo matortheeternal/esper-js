4b31d3ac7c424ee2bd75951d80797a822ca453f7
Colin <why101@msn.com>
2019-11-22 23:04:05 -0800

    minor message tweak for UnknownSignatureError

--- src/errors/UnknownSignatureError.js
+++ src/errors/UnknownSignatureError.js
@@ -1,6 +1,6 @@
 class UnknownSignatureError extends Error {
     constructor(record, sig) {
-        let position = record.memoryMap.getPos().toString(16);
+        let position = record.memoryMap.getPos().toString(16).toUpperCase();
         super(`Encountered unknown signature ${sig} when parsing ` +
             `${record.signature} record at 0+${position}`);
         Error.captureStackTrace(this, UnknownSignatureError);
