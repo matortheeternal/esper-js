2f011ce7c4f6e850763151310799968d51b33295
Colin <why101@msn.com>
2019-11-24 13:23:02 -0800

    added more tests to empty plugin test suite

--- test/integration/empty.test.js
+++ test/integration/empty.test.js
@@ -20,6 +20,83 @@ describe('empty.esp', () => {
         });
 
         describe('members', () => {
+            describe('HEDR', () => {
+                let element;
+
+                beforeAll(() => {
+                    element = emptyFile.fileHeader.getElement('HEDR');
+                });
+
+                it('should be present', () => {
+                    expect(element).toBeDefined();
+                    expect(element.signature).toBe('HEDR');
+                });
+
+                it('should have size', () => {
+                    expect(element.size).toBe(12);
+                });
+
+                describe('Version', () => {
+                    let version;
+
+                    beforeAll(() => {
+                        version = element.getElement('Version');
+                    });
+
+                    it('should be present', () => {
+                        expect(version).toBeDefined();
+                    });
+
+                    it('should have size', () => {
+                        expect(version.size).toBe(4);
+                    });
+
+                    it('should have value', () => {
+                        expect(version.value).toBe('1.70000');
+                    });
+                });
+
+                describe('Number of Records', () => {
+                    let numRecords;
+
+                    beforeAll(() => {
+                        numRecords = element.getElement('Number of Records');
+                    });
+
+                    it('should be present', () => {
+                        expect(numRecords).toBeDefined();
+                    });
+
+                    it('should have size', () => {
+                        expect(numRecords.size).toBe(4);
+                    });
+
+                    it('should have value', () => {
+                        expect(numRecords.value).toBe('0');
+                    });
+                });
+
+                describe('Next Object ID', () => {
+                    let nextId;
+
+                    beforeAll(() => {
+                        nextId = element.getElement('Next Object ID');
+                    });
+
+                    it('should be present', () => {
+                        expect(nextId).toBeDefined();
+                    });
+
+                    it('should have size', () => {
+                        expect(nextId.size).toBe(4);
+                    });
+
+                    it('should have value', () => {
+                        expect(nextId.value).toBe('2048');
+                    });
+                });
+            });
+
             describe('CNAM', () => {
                 let element;
 
