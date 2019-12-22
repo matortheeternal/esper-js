72c4c655f4e5aa368455b2dd10ce795e8c580f52
Colin <why101@msn.com>
2019-12-22 10:38:45 -0800

    added useInstallation and updated loadPlugins in Session

--- src/Session.js
+++ src/Session.js
@@ -1,22 +1,33 @@
 const DefinitionManager = require('./DefinitionManager');
 const PluginManager = require('./PluginManager');
+const path = require('path');
+const assert = require('assert');
 
 class Session {
     constructor(game) {
         this._game = game;
+        this.dataPath = process.cwd();
         this.definitionManager = new DefinitionManager(game);
         this.pluginManager = new PluginManager(game);
     }
 
-    loadPlugins(loadOrder) {
-        loadOrder.forEach(filename => {
-            let filePath = path.resolve(this.dataPath, filename);
-            PluginFile.load(this, filePath);
-        });
+    useInstallation(installation) {
+        assert.strictEqual(installation._game, this._game);
+        if (!installation.isValid())
+            throw new Error('Game installation is not valid.');
+        this.installation = installation;
+        this.dataPath = installation.dataPath;
     }
 
-    get dataPath() {
-        return this._dataPath; // TODO
+    getPluginFilePath(filePath) {
+        if (path.isAbsolute(filePath)) return filePath;
+        return path.resolve(this.dataPath, filePath);
+    }
+
+    loadPlugins(pluginsToLoad) {
+        pluginsToLoad.forEach(filePath => {
+            PluginFile.load(this, this.getPluginFilePath(filePath));
+        });
     }
 }
 
